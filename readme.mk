a9b584e9632facecacc251604925c9dc8f7af682
Alexandre Freitas Duarte <alexfd7@gmail.com>
2023-07-23 00:59:16 -0300

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -1,14 +1,49 @@
-Execução do projeto 
+#  .NET Developer SR - B3.
 
+Um parágrafo da descrição do projeto vai aqui
+
+
+
+### 📋 Pré-requisitos
+
+```
 TargetFramework: net6.0
 
-1ª Opção 
-	Abrir arquivo da solução no Visual Studio, definir como projeto de inicialização e executar projeto
+```
 
-2ª Opção
+## ⚙️ Executando o projeto
 
-	Acessar a pasta do projeto WebApplication
-	cd WebApi
+Explicar como executar os testes automatizados para este sistema.
 
-	Executar comando
-	dotnet watch run --launch-profile WebApiDevelopment
+### 🔩 Analise os testes de ponta a ponta
+
+Explique que eles verificam esses testes e porquê.
+
+```
+Dar exemplos
+```
+
+### ⌨️ E testes de estilo de codificação
+
+Explique que eles verificam esses testes e porquê.
+
+```
+Dar exemplos
+```
+
+## 🛠️ Construído com
+
+Mencione as ferramentas que você usou para criar seu projeto
+
+* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - O framework web usado
+* [Maven](https://maven.apache.org/) - Gerente de Dependência
+* [ROME](https://rometools.github.io/rome/) - Usada para gerar RSS
+
+
+## ✒️ Autores
+
+Mencione todos aqueles que ajudaram a levantar o projeto desde o seu início
+
+[Alexandre Duarte](https://gist.github.com/lohhans) 
+
+---
